55c02c9f07a69be2277e03e0941cac47c34b6f82
Luis Flores <lf016158@gmail.com>
2022-08-27 16:27:14 -0600

    se termino el ejercicio

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,10 +22,130 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtNota1 = New System.Windows.Forms.TextBox()
+        Me.txtNota2 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtPromedio = New System.Windows.Forms.TextBox()
+        Me.txtResultado = New System.Windows.Forms.TextBox()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.btn_Nuevo = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(33, 45)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(42, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nota 1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(33, 80)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(42, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Nota 2"
+        '
+        'txtNota1
+        '
+        Me.txtNota1.Location = New System.Drawing.Point(135, 42)
+        Me.txtNota1.Name = "txtNota1"
+        Me.txtNota1.Size = New System.Drawing.Size(223, 23)
+        Me.txtNota1.TabIndex = 2
+        '
+        'txtNota2
+        '
+        Me.txtNota2.Location = New System.Drawing.Point(135, 77)
+        Me.txtNota2.Name = "txtNota2"
+        Me.txtNota2.Size = New System.Drawing.Size(223, 23)
+        Me.txtNota2.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(33, 123)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(59, 15)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Promedio"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(33, 165)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(96, 15)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Aprovechmiento"
+        '
+        'txtPromedio
+        '
+        Me.txtPromedio.Location = New System.Drawing.Point(135, 120)
+        Me.txtPromedio.Name = "txtPromedio"
+        Me.txtPromedio.Size = New System.Drawing.Size(223, 23)
+        Me.txtPromedio.TabIndex = 6
+        '
+        'txtResultado
+        '
+        Me.txtResultado.Location = New System.Drawing.Point(135, 157)
+        Me.txtResultado.Name = "txtResultado"
+        Me.txtResultado.Size = New System.Drawing.Size(223, 23)
+        Me.txtResultado.TabIndex = 7
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.Location = New System.Drawing.Point(68, 202)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(75, 28)
+        Me.btnCalcular.TabIndex = 8
+        Me.btnCalcular.Text = "Calcular"
+        Me.btnCalcular.UseVisualStyleBackColor = True
+        '
+        'btn_Nuevo
+        '
+        Me.btn_Nuevo.Location = New System.Drawing.Point(253, 202)
+        Me.btn_Nuevo.Name = "btn_Nuevo"
+        Me.btn_Nuevo.Size = New System.Drawing.Size(75, 28)
+        Me.btn_Nuevo.TabIndex = 9
+        Me.btn_Nuevo.Text = "Nuevo"
+        Me.btn_Nuevo.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(426, 242)
+        Me.Controls.Add(Me.btn_Nuevo)
+        Me.Controls.Add(Me.btnCalcular)
+        Me.Controls.Add(Me.txtResultado)
+        Me.Controls.Add(Me.txtPromedio)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtNota2)
+        Me.Controls.Add(Me.txtNota1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Aprovechmiento"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtNota1 As TextBox
+    Friend WithEvents txtNota2 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtPromedio As TextBox
+    Friend WithEvents txtResultado As TextBox
+    Friend WithEvents btnCalcular As Button
+    Friend WithEvents btn_Nuevo As Button
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,44 @@
 ﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
 
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        Try
+            Dim nota1 As Integer
+            Dim nota2 As Integer
+            Dim promedio As Double
+            Dim mensaje As String
+            nota1 = CInt(txtNota1.Text)
+            nota2 = CInt(txtNota2.Text)
+
+            If nota1 < 0 Or nota1 > 100 Then
+                MessageBox.Show("La Nota 1 debe ser mayor o igual a 0 y menor o igual a 100", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            ElseIf nota2 < 0 Or nota2 > 100 Then
+                MessageBox.Show("La Nota 2 debe ser mayor o igual a 0 y menor o igual a 100", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Else
+                promedio = CDbl(((nota1 + nota2) / 2))
+                mensaje = IIf((promedio >= 70), "Aprobado", "Reporbado")
+                txtPromedio.Text = promedio.ToString("00.00") + " %"
+                txtResultado.Text = mensaje
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Ocurrio un Error: " + ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub btn_Nuevo_Click(sender As Object, e As EventArgs) Handles btn_Nuevo.Click
+        txtNota1.Text = ""
+        txtNota2.Text = ""
+        txtPromedio.Text = ""
+        txtResultado.Text = ""
+    End Sub
 End Class
